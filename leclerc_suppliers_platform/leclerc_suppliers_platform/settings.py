a022e71a2523817f2f36fa763ea970d3fbf67d38
Inferi92 <diogojesus92@ua.pt>
2022-05-13 10:22:31 +0100

    static files import

--- leclerc_suppliers_platform/leclerc_suppliers_platform/settings.py
+++ leclerc_suppliers_platform/leclerc_suppliers_platform/settings.py
@@ -10,6 +10,7 @@ For the full list of settings and their values, see
 https://docs.djangoproject.com/en/4.0/ref/settings/
 """
 
+import os
 from pathlib import Path
 
 # Build paths inside the project like this: BASE_DIR / 'subdir'.
@@ -38,6 +39,7 @@ INSTALLED_APPS = [
     'django.contrib.messages',
     'django.contrib.staticfiles',
     "platform_api",
+    "colorfield",
 ]
 
 MIDDLEWARE = [
@@ -119,7 +121,15 @@ USE_TZ = True
 # Static files (CSS, JavaScript, Images)
 # https://docs.djangoproject.com/en/4.0/howto/static-files/
 
-STATIC_URL = 'static/'
+STATIC_ROOT = os.path.join(BASE_DIR, "static")
+
+STATIC_URL = "/static/"
+
+# MEDIA URL
+MEDIA_URL = "/"
+
+# MEDIA ROOT
+MEDIA_ROOT = "platform_api/static/images"
 
 # Default primary key field type
 # https://docs.djangoproject.com/en/4.0/ref/settings/#default-auto-field
