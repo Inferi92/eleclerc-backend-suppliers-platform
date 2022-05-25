9b608f5626296589a61156ef3f7fcab7d6a18a59
Inferi92 <diogojesus92@ua.pt>
2022-05-25 12:36:26 +0100

    cache and clean urls file

--- leclerc_suppliers_platform/platform_api/urls.py
+++ leclerc_suppliers_platform/platform_api/urls.py
@@ -3,7 +3,6 @@ from platform_api import views
 from rest_framework import permissions
 from drf_yasg.views import get_schema_view
 from drf_yasg import openapi
-from django.conf.urls.static import static
 
 schema_view = get_schema_view(
     openapi.Info(
